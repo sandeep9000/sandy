ad70e087439aba414c2076b830ab8b23e77ff2a8
sandeep9000 <s.sandygr8@gmail.com>
2016-10-14 15:04:42 +0100

    Update App.js
    added more json response of weather in UI

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import './App.css';
 
 const App = React.createClass({
   getInitialState: function() {
-    return {data: []};
+    return {data: {
+      list: []
+    }};
   },
 
   componentDidMount: function() {
     fetch('http://api.openweathermap.org/data/2.5/find?q=London&APPID=759bff8904cab98d07e01ce1bff75319')
       .then(r => r.json())
       .then(function(data) {
-        console.log("data", data);
+        console.log("data", data.list);
         this.setState({data: data});
       }.bind(this))
   },
 
   render: function() {
-    return (
+    var data = this.state.data;
+    var a = data.list[3];
+    if(a)
+      {var k = a.main.temp;
+      var p = a.main.temp_max;
+      var m =a.main.temp_min;
+      var w=a.wind.speed;
+      var y=a.wind.deg;
+    }
+      return (
       <div className="commentbox">
-        <h1>Weather</h1>
-        <p>{this.state.data.message}</p>
-      </div>
+        <h1>London Weather</h1>
+        <p><i>{this.state.data.message}</i></p>
+         <ul>ID:{this.state.data.list.map(x =>
+          <li key={x.id}>{x.id}</li>
+         )}</ul>
+         <p><b>Temperature:</b>{k}</p>
+         <p><b>Temperature max:</b>{p}</p>
+        <p> <b>Temperature min:</b>{m}</p>
+        <p> <b>Wind speed:</b>{w}</p>
+        <p> <b>Wind Degree:</b>{y} </p>
+        </div>
     );
   }
 });
